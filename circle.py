67b3b4edf2cc0d0f828d5fb7681c78e3ca307f97
Evgeny <49250650+Monk3y1980@users.noreply.github.com>
2020-10-08 11:56:29 +0200

    add class_circle

---
+++ circle.py
@@ -0,0 +1,6 @@
+from class_circle import Circle
+
+
+r1 = Circle(10)
+
+print(r1.getArea())
---
+++ class_circle.py
@@ -0,0 +1,7 @@
+class Circle:
+	def __init__(self, r):
+		self.r = r
+
+	def getArea(self):
+		pi = 3.1415 
+		return pi * self.r**2
